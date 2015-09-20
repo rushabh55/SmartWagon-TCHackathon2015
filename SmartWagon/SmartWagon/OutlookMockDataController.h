0dd0b814b5514636d708f8bfa5e0b639202b7cff
Gosar, Rushabh <Rushabh_Gosar@intuit.com>
2015-09-20 08:41:15 -0700

    Outlook

---
+++ SmartWagon/SmartWagon/OutlookMockDataController.h
@@ -0,0 +1,13 @@
+//
+//  OutlookMockDataController.h
+//  SmartWagon
+//
+//  Created by Gosar, Rushabh on 9/20/15.
+//  Copyright (c) 2015 Gosar, Rushabh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OutlookMockDataController : NSObject
+
+@end
---
+++ SmartWagon/SmartWagon/OutlookMockDataController.m
@@ -0,0 +1,13 @@
+//
+//  OutlookMockDataController.m
+//  SmartWagon
+//
+//  Created by Gosar, Rushabh on 9/20/15.
+//  Copyright (c) 2015 Gosar, Rushabh. All rights reserved.
+//
+
+#import "OutlookMockDataController.h"
+
+@implementation OutlookMockDataController
+
+@end
