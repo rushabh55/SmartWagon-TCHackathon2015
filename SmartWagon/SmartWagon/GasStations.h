63c4ebba2876bbba9d9c7d7a53c98bafb1114caf
Siddesh Pillai <siddeshvijji@gmail.com>
2015-09-20 07:38:07 -0400

    Gas-station updated

---
+++ SmartWagon/SmartWagon/GasStations.h
@@ -0,0 +1,13 @@
+//
+//  GasStations.h
+//  SmartWagon
+//
+//  Created by Hitesh Vyas on 9/20/15.
+//  Copyright © 2015 Gosar, Rushabh. All rights reserved.
+//
+
+#ifndef GasStations_h
+#define GasStations_h
+
+
+#endif /* GasStations_h */
