4a7c6f62f5bbbff208d14a8e1ee9447f78d54e7b
Hitesh Vyas <hv5451@rit.edu>
2015-09-20 02:10:17 -0400

    Adding Esri SDK

---
+++ SmartWagon/SmartWagon/EsriNavigator.h
@@ -0,0 +1,13 @@
+//
+//  EsriNavigator.h
+//  SmartWagon
+//
+//  Created by Hitesh Vyas on 9/20/15.
+//  Copyright © 2015 Gosar, Rushabh. All rights reserved.
+//
+
+#ifndef EsriNavigator_h
+#define EsriNavigator_h
+#import <ArcGIS/ArcGIS.h>
+
+#endif /* EsriNavigator_h */
