7c3f22054bd9beeb16d944ae5bf90530adc73309
Siddesh Pillai <siddeshvijji@gmail.com>
2015-09-20 04:25:46 -0400

    Mocked Vinli API stuff.

--- SmartWagon/SmartWagon/MockVinliDataControl.m
+++ SmartWagon/SmartWagon/MockVinliDataControl.m
@@ -12,10 +12,18 @@
 @implementation MockVinliDataControl
 -(NSDictionary*)mockData {
     NSMutableDictionary* dict = [[NSMutableDictionary alloc] init];
-    
-    [dict setObject:@"value" forKey:@"key"];
-    
+    [dict setObject:@"13" forKey:@"Mileage"];
+    [dict setObject:@"1.87" forKey:@"AVG_MPG"];
+    [dict setObject:@"15.55" forKey:@"MAX_MPG"];
+    [dict setObject:@"64.54" forKey:@"FUEL_USED"];
+    [dict setObject:@"964" forKey:@"MILES_TRAVELED"];
+    [dict setObject:@"158.45" forKey:@"AVG_TRIP_SPEED"];
+    [dict setObject:@"40.80" forKey:@"AVG_SPEED"];
+    [dict setObject:@"1" forKey:@"HARD_ACCEL"];
+    [dict setObject:@"585-xxx-7918" forKey:@"EMERGENCY_CONTACT"];    
     return dict;
 }
+
+
 @end
 
