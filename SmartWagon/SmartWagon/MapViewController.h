46ff215c0e77ae6348bcc3f2787b401e55212862
Gosar, Rushabh <Rushabh_Gosar@intuit.com>
2015-09-20 01:00:52 -0700

    Maps in!

---
+++ SmartWagon/SmartWagon/MapViewController.h
@@ -0,0 +1,15 @@
+//
+//  MapViewController.h
+//  SmartWagon
+//
+//  Created by Gosar, Rushabh on 9/19/15.
+//  Copyright (c) 2015 Gosar, Rushabh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <ArcGIS/ArcGIS.h>
+
+@interface MapViewController : UIViewController<AGSMapViewLayerDelegate, CLLocationManagerDelegate, UIAlertViewDelegate>
+@property (weak, nonatomic) IBOutlet AGSMapView *mapView;
+
+@end
